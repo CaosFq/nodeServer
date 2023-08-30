b75bef7f0c337cb34eedd1bf2aac179e94e02282
CaosFq <andresromerofq@gmail.com>
2023-08-30 19:40:26 +0200

    second

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+//Imports
 const express = require("express");
 const path = require("path");
 const app = express();
+const engine = require("express-edge");
 
+// Configure view caching
+app.enable("view cache");
+// --- or ---
+//app.diable("view cache");
+
+//Settings
 const port = 5000;
+
+const courses = [
+  {
+    title: "Desarrollo web con JavaScript",
+    description: "Full Stack desde cero",
+  },
+  {
+    title: "HTML y CSS Desde cero",
+    description: "Crea webs modernas",
+  },
+  {
+    title: "VueJS Desde cero",
+    description: "Framework moderno de JS",
+  },
+];
+app.set("views", path.join(__dirname, "views"));
+
+//Middlewares
+app.use(engine);
 app.use(express.static("public"));
 app.use((req, res, next) => {
   console.log(`
@@ -13,13 +40,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get(
-  "/",
+//Routes
 
-  (req, res) => {
-    res.sendFile(path.join(__dirname, "views/app.html"));
-  }
-);
+app.get("/", (req, res) => {
+  res.render("app");
+});
 app.listen(port, () => {
   console.log(`App running on port ${port}🙌`);
 });
